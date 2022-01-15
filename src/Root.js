e35154fe73d26aa478d9793b59e65204586166f7
Jakub Jóźwiak <jakubjozwiak@Jakub-MacBook-Air.local>
2022-01-15 23:34:50 +0100

    change direction of globalstyle file

--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,9 @@
-import { createGlobalStyle } from 'styled-components';
 import { PostBoard } from './components/organisms/PostBoard';
 import { theme } from './assets/styles/theme';
 import { ThemeProvider } from 'styled-components';
 import ArticleProvider from './Providers/ArticleProvider';
+import { GlobalStyle } from './assets/styles/globalStyle';
 
-const GlobalStyle = createGlobalStyle`
-
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@300;400;600&display=swap');
-
-  body{
-    margin: 0;
-    padding: 0;
-    font-family: 'Poppins', sans-serif;
-  }
-`;
 
 const Root = () => {
   return (
---
+++ src/assets/styles/globalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@300;400;600&display=swap');
+
+  body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+`;
